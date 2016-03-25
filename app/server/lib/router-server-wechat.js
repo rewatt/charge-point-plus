f02e903ec34d0d88c065dbe3190ef6100e073f66
zengzhou <jzwzz@163.com>
2016-03-25 04:07:06 -0400

    add wechat server address confirm api

---
+++ app/server/lib/router-server-wechat.js
@@ -0,0 +1,33 @@
+Picker.route('/wechat/post/:_id', function(params, req, res, next) {
+    res.end(params._id + ', id.');
+});
+
+var SHA1 = Npm.require('crypto-js/sha1');
+
+/* wechat api - verify host */
+Picker.route('/wechat/callback/verifyhost', function(params, req, res, next) {
+    var signature = params.signature;
+    var timestamp = params.timestamp;
+    var nonce = params.nonce;
+    var token = params.token;
+
+    var echostr = params.echostr;
+    var tmpStr = [token, timestamp, nonce].sort().join('');
+
+    tmpStr = SHA1(tmpStr).toString();
+
+    console.log('tmpStr1:' + tmpStr);
+    console.log('signature:'+ signature);
+    console.log('timestamp:'+ timestamp);
+    console.log('nonce:'+ nonce);
+    console.log('token:'+ token);
+    console.log('tmpStr:' + tmpStr);
+    console.log('echostr:'+ echostr);
+
+    var resultStr = '';
+    if(signature == tmpStr){
+        resultStr = echostr;
+    }
+    console.log ('resultStr:' + resultStr)
+    res.end(resultStr);
+});
--- docs/meteor-resources.md
+++ docs/meteor-resources.md
@@ -1,1 +1,16 @@
 https://atmospherejs.com/
+
+
+
+
+
+
+https://github.com/Ethaan/playground
+
+https://github.com/thereactivestack/kickstart-meteor-react-flowrouter
+
+
+
+# update to new version
+# the new vesion reference  https://github.com/meteor/meteor/releases
+meteor update --release 1.3-rc.12
